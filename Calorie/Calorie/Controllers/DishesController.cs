4a71349564542d5815cc37166863863cccd49d93
dimapct <dimapct@gmail.com>
2015-10-18 01:25:35 +0300

    Moved to auto-generated controllers

---
+++ Calorie/Calorie/Controllers/DishesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Calorie.Models;
+
+namespace Calorie.Controllers
+{
+    public class DishesController : Controller
+    {
+        private CalorieModel db = new CalorieModel();
+
+        // GET: Dishes
+        public ActionResult Index()
+        {
+            return View(db.Dishes.ToList());
+        }
+
+        // GET: Dishes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dishes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DishID,DishName")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dishes.Add(dish);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dish);
+        }
+
+        // GET: Dishes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: Dishes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DishID,DishName")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dish).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dish);
+        }
+
+        // GET: Dishes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = db.Dishes.Find(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: Dishes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Dish dish = db.Dishes.Find(id);
+            db.Dishes.Remove(dish);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
